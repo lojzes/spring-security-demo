ce9cd53026f101a46b4ae62c469b93ee4e48cbc5
lojzes <lyyitit@foxmail.com>
2019-10-24 23:55:19 +0800

    lyy

---
+++ oauth2-client/src/main/java/com/lojzes/oauth/client/Oauth2ClientApp.java
@@ -0,0 +1,11 @@
+package com.lojzes.oauth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientApp {
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2ClientApp.class, args);
+  }
+}
---
+++ oauth2-client/src/main/resources/url.md
@@ -0,0 +1,7 @@
+http://39itg7.natappfree.cc/login/oauth2/code/github
+
+
+55e44375e72d502e1f3490ac673899d04a8aebbe
+c0fbadecb924372bc195b162f2a3c4cd8c71d425
+
+16240795
